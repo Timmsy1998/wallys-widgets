000ceda07bfa23f68a39eeefdfbe02c0dc4d6b6c
James Timms <59345798+Timmsy1998@users.noreply.github.com>
2023-11-06 15:51:11 +0000

    Added Vue to the Laravel Instance

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,39 +1,20 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+// Import necessary dependencies and configuration files.
+import "./bootstrap"; // Include Javascript Bootstrap
+import { createApp } from "vue"; // Create a Vue 3 app instance
+import { createRouter, createWebHistory } from "vue-router"; // Set up Vue Router
 
-import './bootstrap';
-import { createApp } from 'vue';
+// Import the root Vue component.
+import App from "./components/App.vue";
 
-/**
- * Next, we will create a fresh Vue application instance. You may then begin
- * registering components with the application instance so they are ready
- * to use in your application's views. An example is included for you.
- */
+// Create a new Vue Router instance.
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    // Define the routes for the application
+  ]
+});
 
-const app = createApp({});
-
-import ExampleComponent from './components/ExampleComponent.vue';
-app.component('example-component', ExampleComponent);
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// Object.entries(import.meta.glob('./**/*.vue', { eager: true })).forEach(([path, definition]) => {
-//     app.component(path.split('/').pop().replace(/\.\w+$/, ''), definition.default);
-// });
-
-/**
- * Finally, we will attach the application instance to a HTML element with
- * an "id" attribute of "app". This element is included with the "auth"
- * scaffolding. Otherwise, you will need to add an element yourself.
- */
-
-app.mount('#app');
+// Create a new Vue app instance and configure it with Vue Router.
+const app = createApp(App);
+app.use(router); // Use the Vue Router in the app
+app.mount("#app"); // Mount the app to the HTML element with id 'app'
---
+++ resources/js/components/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <header>
+            <div class="px-3 py-2 bg-dark text-white">
+                <div class="container">
+                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                        <!-- Logo and navigation links -->
+                        <router-link to="/"
+                            class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+                            <img src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png"
+                                style="height:60px;" />
+                        </router-link>
+                        <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                            <!-- Home link -->
+                            <li>
+                                <router-link to="/" class="nav-link text-white"
+                                    style="text-decoration: none; color: white;">
+                                    <svg class="bi d-block mx-auto mb-1" xmlns="http://www.w3.org/2000/svg" width="24"
+                                        height="24" viewBox="0 0 24 24">
+                                        <path fill="currentColor"
+                                            d="M4.5 21q-.625 0-1.063-.438T3 19.5v-1.9l4-3.55V21H4.5ZM8 21v-4h8v4H8Zm9 0v-8.2L12.725 9l3.025-2.675l4.75 4.225q.25.225.375.513t.125.612V19.5q0 .625-.438 1.063T19.5 21H17ZM3 16.25v-4.575q0-.325.125-.625t.375-.5L11 3.9q.2-.2.463-.287T12 3.525q.275 0.537.088T13 3.9l2 1.775L3 16.25Z" />
+                                    </svg>Home
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Content container -->
+        <div class="card">
+            <div class="card-body">
+                <div class="container-fluid">
+                    <!-- Display the Vue Router view -->
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'  // The name of the Vue component
+}
+</script>
